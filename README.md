7fe0f1e11eec66c60861449808e082e73d5e428d
serial3 <diogo_R_Leal@hotmail.com>
2021-10-27 18:43:35 -0700

    lstnew added

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Still wondering how i'll store all the projects, wether everything on this repos
 Missing:
 
 - ft_split.c
-- all the bonuses.
+- all the bonuses except ft_lstnew.c.
 
 Missing most headers, will fix norme once i go to school.
 
--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -1,27 +1,27 @@
 #include "libft.h"
 
-void	ft_putnbr_fd(int nb, int fd)
+void	ft_putnbr_fd(int n, int fd)
 {
-	if (nb < 0)
+	if (n < 0)
 	{
-		if (nb == -2147483648)
+		if (n == -2147483648)
 		{
 			ft_putnbr_fd (-21, fd);
 			ft_putnbr_fd (47483648, fd);
 			return ;
 		}
 		ft_putchar_fd('-', fd);
-		nb = nb * -1;
+		n = n * -1;
 	}
-	if (nb >= 0 && nb < 10)
+	if (n >= 0 && n < 10)
 	{
-		nb = nb + '0';
-		ft_putchar_fd(nb, fd);
+		n = n + '0';
+		ft_putchar_fd(n, fd);
 	}
 	else
 	{
-		ft_putnbr_fd (nb / 10, fd);
-		nb = nb % 10 + '0';
-		ft_putchar_fd(nb, fd);
+		ft_putnbr_fd (n / 10, fd);
+		n = n % 10 + '0';
+		ft_putchar_fd(n, fd);
 	}
 }
